a70db671676f4c91372bc681b092b90fc5a55ef2
hoangviet2 <hoangviet18306@gmail.com>
2021-07-30 09:51:01 +0700

    Re change

--- English Dictionary/English.py
+++ English Dictionary/English.py
@@ -1,12 +1,17 @@
 import json
 from difflib import get_close_matches
+from os import pipe
 data = json.load(open("data.json"))
 def translate(word):
     if word.lower() in data:
         return data[word.lower()]
     elif len(get_close_matches(word,data.keys()))>0:
-        return "Did you mean %s instead" %get_close_matches(word,data.keys())[0]
+        yn = input("Did you mean %s instead" %get_close_matches(word,data.keys())[0])
+        if yn == 'Y':
+            return get_close_matches(word,data.keys())[0]
     else:
         return "word doesn't exists. Please double check it"
 word = input("Enter word: ")
-print(translate(word))
+output = translate(word)
+for items in output:
+    print(items)
