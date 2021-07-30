08276f08648e6bfee668b6e8ce3611aa416911eb
hoangviet2 <hoangviet18306@gmail.com>
2021-07-30 17:50:24 +0700

    Finish
    Finish up the English Dictionary app

--- English Dictionary/English.py
+++ English Dictionary/English.py
@@ -5,13 +5,24 @@ data = json.load(open("data.json"))
 def translate(word):
     if word.lower() in data:
         return data[word.lower()]
+    elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word,data.keys()))>0:
-        yn = input("Did you mean %s instead" %get_close_matches(word,data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y for Yes and N for No " %get_close_matches(word,data.keys())[0])
         if yn == 'Y':
-            return get_close_matches(word,data.keys())[0]
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn == 'N':
+            return "word doesn't exists. Please double check it"
+        else:
+            return "we didn't understand you"
     else:
         return "word doesn't exists. Please double check it"
 word = input("Enter word: ")
 output = translate(word)
-for items in output:
-    print(items)
+if type(output) == list:
+    for items in output:
+        print(items)
+else:
+    print(output)
